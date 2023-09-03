f6ee908e09f26995d0c73222c053e8ec3682e5d0
UbongJacob <ubongjacob13@gmail.com>
2023-09-03 10:55:57 +0100

    Handled post request validation with joi

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const Joi = require("joi");
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "course1" },
@@ -9,11 +12,31 @@ const courses = [
 ];
 
 app.get("/", (req, res) => {
-  res.send("Hello WorlSssd");
+  res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
   res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+
+  res.status(201).send(course);
 });
 
 app.get("/api/courses/:id", (req, res) => {
@@ -26,6 +49,6 @@ app.get("/api/courses/:id", (req, res) => {
   } else res.send(course);
 });
 
+//
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => console.log(`Listening on port ${port}...`));
