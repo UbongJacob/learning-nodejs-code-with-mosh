1f273ee25b30f80d6440bb84c51b14d5b1b658e8
UbongJacob <ubongjacob13@gmail.com>
2023-09-03 08:41:27 +0100

    Learnt how to handle HTTP get request

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require("express");
 
 const app = express();
 
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello WorlSssd");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.get("/api/courses/:id", (req, res) => {
+  const reqID = req.params.id;
+  const course = courses.find((course) => course.id === parseInt(reqID));
+  if (!course) {
+    res
+      .status(404)
+      .send(`The course with the given ID ${reqID} was not found.`); //404
+  } else res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
